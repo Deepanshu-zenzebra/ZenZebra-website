773ba343b076d0544b13c851216179c8684bcbcb
Ketan Gulati <ketangulati66@gmail.com>
2025-11-03 14:18:40 +0530

    updated site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,6 +6,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: 'https://zenzebra.in/', lastModified: new Date() },
     { url: 'https://zenzebra.in/brands', lastModified: new Date() },
     { url: 'https://zenzebra.in/partners', lastModified: new Date() },
-    { url: 'https://zenzebra.in/catalog' , lastModified: new Date() },
+    { url: 'https://zenzebra.in/catalogue' , lastModified: new Date() },
+    { url: 'https://zenzebra.in/contact' , lastModified: new Date() },
+    { url: 'https://zenzebra.in/zen-at-home' , lastModified: new Date() },
+
   ];
 }
